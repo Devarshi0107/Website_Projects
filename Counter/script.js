788adf0b55b4651b642fe007b564c9417abdac4e
Devarshi0107 <143324272+Devarshi0107@users.noreply.github.com>
2023-12-14 19:49:15 +0530

    Add files via upload

---
+++ Counter/script.js
@@ -0,0 +1,15 @@
+let c=0;
+// let input=document.getElementById("no");
+
+function rfxn(){
+    c =0;
+    document.getElementById("no").innerHTML=c;
+}
+function dfxn(){
+    c -=1;
+    document.getElementById("no").innerHTML=c;
+}
+function ifxn(){
+    c +=1;
+    document.getElementById("no").innerHTML=c;
+}
